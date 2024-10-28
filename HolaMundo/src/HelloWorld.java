bcb803409c3691c2fc955d070ff6da71aea65359
luis-pc\luis <vluis5457@gmail.com>
2024-10-28 14:36:54 -0600

    Ejemplos de variables

--- HolaMundo/src/HelloWorld.java
+++ HolaMundo/src/HelloWorld.java
@@ -2,8 +2,30 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Luis Vásquez");
+        // VARIABLES
 
+        // STRING
+        String helloWorld = "Hello World!!";
+        System.out.println(helloWorld);
+
+        // INTEGER
+        int number = 10;
+        System.out.println("number = " + number);
+
+        // BOOLEAN
+        boolean value = false;
+
+        if(!value) {
+            System.out.println("value = " + value);
+        }else{
+            System.out.println("value = " + value);
+        }
+
+        // DYNAMIC VARIABLES
+        var name = "John";
+        System.out.println("name = " + name);
+
+        
     }
 
 }
